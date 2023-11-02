be5fcd7d0edd3c9eb1f29f8cb5bf5c0706dfd0f1
P. John Ruben Raj <pjohn21@karunya.edu.in>
2023-11-02 15:22:16 +0530

    Revert "commit final"
    This reverts commit b733c792114db4c3423cc79417b2c7e9f5f573a4.

--- config/app.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  database: process.env.DB_URL,
-  token: {
-    secret: process.env.JWT_STRATEGY_SECRET
-  },
-
-};
--- jwt.js
+++
@@ -1,3 +0,0 @@
-const crypto = require('crypto');
-const jwtSecret = crypto.randomBytes(32).toString('hex');
-console.log('JWT Secret Key:', jwtSecret);
